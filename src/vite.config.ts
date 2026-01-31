e4f2ebef09ea44766e7fe137c795a44d116614b3
eancalapit <163398301+eancalapit@users.noreply.github.com>
2026-02-01 05:12:00 +0800

    Update vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/for-my-talks-a-lot",
+  base: "/for-my-talks-a-lot/",
   plugins: [react()],
 });
